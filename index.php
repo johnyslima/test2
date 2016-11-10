336469fdf1a54447f522bcd07c474fe58ac416e3
johnyslima <johnyslima@yandex.ru>
2016-11-10 22:51:39 +0500

    update title

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Main</title>
+        <title>Main2</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="js/vendor/jquery-ui-1.12.1.custom/jquery-ui.min.css">
